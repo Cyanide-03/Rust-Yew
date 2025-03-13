fa1825363e8a684f9c67cdd3f7a85fb939bf18ba
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-13 17:27:28 +0000

    Add web-sys dependency and update main component to handle user input

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,52 @@
 use yew::prelude::*;
 use yew::Renderer;
 
-#[function_component(Counter)]
-fn counter() -> Html {
-    let count = use_state(|| 0);
+// #[function_component(Counter)]
+// fn counter() -> Html {
+//     let count = use_state(|| 0);
 
-    let increase = {
-        let count = count.clone();
-        Callback::from(move |_| count.set(*count + 1))
-    };
+//     let increase = {
+//         let count = count.clone();
+//         Callback::from(move |_| count.set(*count + 1))
+//     };
 
-    let decrease = {
-        let count = count.clone();
-        Callback::from(move |_| count.set(*count - 1))
-    };
+//     let decrease = {
+//         let count = count.clone();
+//         Callback::from(move |_| count.set(*count - 1))
+//     };
 
-    html! {
-        <div>
-            <h1>{ "Yew Counter App" }</h1>
-            <p>{ format!("Count: {}", *count) }</p>
-            <button onclick={increase}>{ "Increment" }</button>
-            <button onclick={decrease}>{ "Decrement" }</button>
-        </div>
-    }
-}x
+//     html! {
+//         <div>
+//             <h1>{ "Yew Counter App" }</h1>
+//             <p>{ format!("Count: {}", *count) }</p>
+//             <button onclick={increase}>{ "Increment" }</button>
+//             <button onclick={decrease}>{ "Decrement" }</button>
+//         </div>
+//     }
+// }
 
 #[function_component(App)]
 fn app() -> Html {
-    html! { <Counter /> }
+    // html! { <Counter /> }
+    let name=use_state(|| String::from(""));
+
+    let on_input={
+        let name=name.clone();
+        Callback::from(move |e:InputEvent|{
+            let input:web_sys::HtmlInputElement=e.target_dyn_into().unwrap();
+            name.set(input.value());
+        })
+    };
+
+    html!{
+        <div>
+            <h1>{"Hello, Yew!"}</h1>
+            <input type="text" placeholder="Enter your name" oninput={on_input} />
+            <p>{format!("Hello, {}!",*name)}</p>
+        </div>
+    }
 }
 
 fn main() {
-    Renderer::<App>::new().render();
+    yew::Renderer::<App>::new().render();
 }
