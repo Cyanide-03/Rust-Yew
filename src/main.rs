8e5757f3ece5e770138ada0eeaa153335f47f05b
Cyanide-03 <aditya.ag0301@gmail.com>
2025-03-15 04:56:39 +0000

    Update index.html and main.rs to modify app structure and button functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use yew::prelude::*;
 use yew::Renderer;
 
+// ! For an app which increments and decrements a counter
 // #[function_component(Counter)]
 // fn counter() -> Html {
 //     let count = use_state(|| 0);
@@ -25,24 +26,51 @@ use yew::Renderer;
 //     }
 // }
 
+
+// ! For an app which takes user input and displays a greeting
+// #[function_component(App)]
+// fn app() -> Html {
+    // html! { <Counter /> }
+// }
+
+// #[function_component(App)]
+// fn app() -> Html {
+//     let name=use_state(|| String::from(""));
+
+//     let on_input={
+//         let name=name.clone();
+//         Callback::from(move |e:InputEvent|{
+//             let input:web_sys::HtmlInputElement=e.target_dyn_into().unwrap();
+//             name.set(input.value());
+//         })
+//     };
+
+//     html!{
+//         <div>
+//             <h1>{"Hello, Yew!"}</h1>
+//             <input type="text" placeholder="Enter your name" oninput={on_input} />
+//             <p>{format!("Hello, {}!",*name)}</p>
+//         </div>
+//     }
+// }
+
+// ! For an app which let the user click a button and display a message
 #[function_component(App)]
 fn app() -> Html {
-    // html! { <Counter /> }
-    let name=use_state(|| String::from(""));
+    let message=use_state(|| String::new());
 
-    let on_input={
-        let name=name.clone();
-        Callback::from(move |e:InputEvent|{
-            let input:web_sys::HtmlInputElement=e.target_dyn_into().unwrap();
-            name.set(input.value());
+    let on_click={
+        let message=message.clone();
+        Callback::from(move |_|{
+            message.set("You clicked the button!".to_string());
         })
     };
 
     html!{
         <div>
             <h1>{"Hello, Yew!"}</h1>
-            <input type="text" placeholder="Enter your name" oninput={on_input} />
-            <p>{format!("Hello, {}!",*name)}</p>
+            <button onclick={on_click}>{ "Click me!" }</button>
+            <p>{(*message).clone()}</p>
         </div>
     }
 }
